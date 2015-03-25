92fa3aec5cf9b7a9567f141a6452952644859d2e
unknown <ra25544@an098084-ct107v.nvstu.edu>
2015-03-25 14:55:22 -0400

    Odd Number

---
+++ Odd.java
@@ -0,0 +1,11 @@
+public class Odd {
+	public static void main(String[]args){
+
+
+	for(int i=0; i<100; i++){
+		if ( (i % 2 !=0) && i!=5 && i!=9 && i!=11){
+		System.out.println(i);
+	}
+	 }
+		}
+		 }
